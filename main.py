3f90167860461293a4d1cf226f3dd189345578c0
Bintang Pradana <bintangpradana02@gmail.com>
2020-12-02 11:02:19 +0700

    rework everything

--- main.py
+++ main.py
@@ -1,77 +1,82 @@
-import math
 import random
 
-
 class LVQ:
-  '''
-  Inisialisasi dengan data (x, y), learning rate, dan epoch
-  X merupakan data dan Y merupakan target
-  '''
-  def __init__(self, x, y, lr, epoch):
-    self.x = x
-    self.y = y
-    self.w = [None, x[0].copy(), x[1].copy()]
-    self.lr = lr
-    self.epoch = epoch
+  def __init__(self):
+    self.X = []
+    self.y = []
+    self.w = []
+    self.lr = 0
+    self.epoch = 0
+  
+  def load_data(self, X_test, y_test):
+    self.X = X_test
+    self.y = y_test
 
-  '''
-  Fungsi yang digunakan untuk menghitung jarak antara x1 dan x2
-  '''
-  def dist(self, x1, x2):
-    temp = 0
-    for x1_, x2_ in zip(x1, x2):
-      temp += (x1_ - x2_) ** 2
-    return math.sqrt(temp)
+  def init_weight(self):
+    self.w = [[random.random() for _ in self.X[0]] for _ in range(2)]
 
-  '''
-  Fungsi yang digunakan untuk membandingkan antara input dengan weight 1 dan weight 2
-  Jika jarak input dengan weight 1 lebih kecil dari jarak input dengan weight 2 maka me-return 1
-  Jika jarak input dengan weight 2 lebih kecil dari jarak input dengan weight 1 maka me-return 2
-  Jika jarak input dengan weight 1 sama dengan jarak input dengan weight 2 maka me-return random antara 1 dan 2
-  Jika nilai nilai 
-  '''
+  def distance(self, a, b):
+    total = 0
+    for a_, b_ in zip(a, b):
+      total += (a_ - b_) ** 2
+    dist = total ** 0.5
+    return dist
+
   def compare(self, x):
-    class1 = self.dist(x, self.w[1])
-    class2 = self.dist(x, self.w[2])
-    if class1 < class2:
-      return 1
-    elif class2 < class1:
-      return 2
-    elif class2 == class1:
-      return random.randint(1,2)
+    dist = []
+    for w_ in self.w:
+      dist.append(self.distance(x, w_))
+    return dist.index(min(dist))
 
-  '''
-  Fungsi yang digunakan untuk melakukan iterasi pada data sesuai dengan algoritma LVQ
-  '''
   def iterate(self):
-    for x_, y_ in zip(self.x, self.y):
+    confusion = [0, 0, 0, 0]
+
+    for x_, y_ in zip(self.X, self.y):
       cluster = self.compare(x_)
       if cluster == y_:
+        if cluster == 0:
+          confusion[0] += 1
+        if cluster == 1:
+          confusion[3] += 1
         for i in range(len(self.w[cluster])):
           self.w[cluster][i] = self.w[cluster][i] + self.lr*(x_[i] - self.w[cluster][i])
+          
       elif cluster != y_:
+        if cluster == 0:
+          confusion[2] += 1
+        if cluster == 1:
+          confusion[1] += 1
         for i in range(len(self.w[cluster])):
           self.w[cluster][i] = self.w[cluster][i] - self.lr*(x_[i] - self.w[cluster][i])
-      print(x_, y_, cluster)
+      
+    return confusion
 
-  '''
-  Fungsi untuk menjalankan algoritma LVQ sebanyak n epoch
-  '''
-  def run(self):
+  def score(self, conf):
+    accuracy = (conf[0]+conf[3]) / (conf[0]+conf[1]+conf[2]+conf[3])
+    recall = conf[3] / (conf[3]+conf[2])
+    precision = conf[3] / (conf[3]+conf[1])
+    fpr = conf[2] / (conf[0]+conf[2])
+    f1 = 2*(precision*recall)/(precision+recall)
+
+    return accuracy, recall, precision, fpr, f1
+
+  def train(self, lr, epoch, verbose=False):
+    self.lr = lr
+    self.epoch = epoch
     for i in range(self.epoch):
       print('Epoch %d/%d' % (i+1, self.epoch))
-      self.iterate()
-      '''
-      Mencetak weight 1 dan weight 2
-      '''
-      print('weight 1 :', self.w[1])
-      print('weight 2 :', self.w[2])
+      conf = self.iterate()
+      
+      if verbose:
+        accuracy, recall, precision, fpr, f1 = self.score(conf)
+        print('Accuracy:', accuracy)
+        print('Recall:', recall)
+        print('Precision:', precision)
+        print('FPR:', fpr)
+        print('F1:', f1)
 
 
 if __name__ == '__main__':
-  '''
-  Inisialisasi data
-  '''
   x = [
     [1, 0, 0, 0, 1, 0],
     [0, 1, 1, 1, 1, 0],
@@ -84,14 +89,11 @@ if __name__ == '__main__':
     [1, 0, 0, 1, 0, 1],
     [0, 1, 1, 1, 1, 1]
   ]
-  y = [1, 2, 1, 1, 1, 1, 2, 2, 2, 2]
+  y = [0, 1, 0, 0, 0, 0, 1, 1, 1, 1]
+  lr = 0.001
+  epoch = 3
 
-  '''
-  Inisialisasi objek LVQ
-  '''
-  lvq = LVQ(x, y, 0.05, 3)
-
-  '''
-  Menjalankan algoritma LVQ
-  '''
-  lvq.run()
+  lvq = LVQ()
+  lvq.load_data(x, y)
+  lvq.init_weight()
+  lvq.train(lr, epoch, verbose=True)
